0331e29245e34cf582267a6c0838995355badc8f
“Danilo <danilo.antunes91@gmail.com>
2023-06-21 18:39:59 +0200

    Solved Lab

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + "/views/about.html");
+});
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/home.html");
+});
+
+app.get("/works", (req, res) => {
+  res.sendFile(__dirname + "/views/works.html");
+});
+
+app.get("/gallery", (req, res) => {
+  res.sendFile(__dirname + "/views/gallery.html");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
